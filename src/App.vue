3fe117be72845b5035cb2eeafe73f6fc4b9850c6
Isaac Mbira <inm206@nyu.edu>
2022-06-17 21:54:48 +1000

    App setup: installed axios and tailwind + config

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">
+      Hello 👋🏼
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
